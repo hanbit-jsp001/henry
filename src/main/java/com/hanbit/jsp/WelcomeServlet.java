30d3b501c664635c60cefdc109ed848a8e87c3fe
HB07-10 <HB07-10@HB07_10>
2016-11-19 17:39:32 +0900

    최초커밋

---
+++ src/main/java/com/hanbit/jsp/WelcomeServlet.java
@@ -0,0 +1,29 @@
+package com.hanbit.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet{
+	@Override
+	/*
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.getOutputStream().println("Welcome Get");
+		resp.flushBuffer();
+	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.getOutputStream().println("Welcome Post");
+		resp.flushBuffer();
+	}
+	*/
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.getOutputStream().println("Welcome: "+req.getMethod());
+		resp.flushBuffer();
+	}
+}
